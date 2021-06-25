0da792a6c175281616980e5c47b8c89e9bace7de
professional-pigeon <kyle.kayperez@gmail.com>
2021-06-25 09:17:02 -0700

    create test for venus age, it will fail

--- __tests__/galacticAge.tests.js
+++ __tests__/galacticAge.tests.js
@@ -18,7 +18,9 @@ describe('Person', () => {
       expect(user.mercuryAge()).toEqual(208)
     })
 
-    // test('should give you your Venus age', () => {
-    //   expect(user.mercuryAge()).toEqual(31)
-    // })
+    test('should give you your Venus age', () => {
+      expect(user.venusAge()).toEqual(161)
+    })
+
+    
 });
