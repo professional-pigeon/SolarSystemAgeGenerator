820a89092a463277b4b6e9bd176d461ac77bbf4d
professional-pigeon <kyle.kayperez@gmail.com>
2021-06-25 08:42:02 -0700

    create first test, fail it

---
+++ __tests__/galacticAge.tests.js
@@ -0,0 +1,15 @@
+  
+import User from '../src/galacticAge.js';
+
+describe('Character', () => {
+
+  let User;
+    
+    beforeEach(() =>  {
+      User = new User("Kyle", 32);
+    });
+
+    test('should create a User object',  () => {
+      expect(user.name).toEqual("Kyle");
+      expect(user.age).toEqual(32)
+    });
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './css/styles.css';
+// // import Character from './rpg.js';
+
+// let age = 100
+// let mercuryAge = age * .24
+// let venusAge = age * .62
+// let marsAge = age * 1.88
+// let jupiterAge = age * 11.86
+
+// ex
+// let saturnAge = age * 29.46
+// let uranusAge = age * 84.01
+// let neptuneAge = age * 164.79
+// let plutoAge = age * 248.59
